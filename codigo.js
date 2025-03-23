68125f88cde3d82ddb85c1a30fdd0a18c8ed4ca6
MartinZilber <48520155@est.ort.edu.ar>
2025-03-23 19:13:16 -0300

    Trabajo finalizado - 1.0

--- codigo.js
+++ codigo.js
@@ -1,67 +1,155 @@
 const notaMinima = 1, notaMaxima = 10;
 const formulario = document.getElementById('formulario');
-const notaMatematica = document.getElementById('notaMatematica');
-const notaLengua = document.getElementById('notaLengua');
-const notaEFSI = document.getElementById('notaEFSI');
-const alerta = document.getElementById('alerta');
+let notaMatematica = document.getElementById('matematica');
+let notaLengua = document.getElementById('lengua');
+let notaEFSI = document.getElementById('EFSI');
 const promedioTexto = document.getElementById('promedio');
 const mayorNota = document.getElementById('mayorNota');
 let notasValidas = false;
-
-function onChange(id){
-    promedio.innerText = "";
-    let valor = document.getElementById(id);
-    const valorNumerico = parseInt(valor.value);
-    console.log(valorNumerico);
-    if ((isNaN(valorNumerico) || valorNumerico < notaMinima || valorNumerico > notaMaxima))
+let alerta = document.getElementById('alerta');
+let mensajeAlerta = "Los valores deben ser númericos, mayores o iguales a " + notaMinima + " y menores o iguales a " + notaMaxima;
+const botonPromedio = document.getElementById('calcularPromedio');
+const botonNota = document.getElementById('calcularMayorNota');
+const promedioTag = document.getElementById('promedio');
+const mayorNotaTag = document.getElementById('mayorNota');
+const divPromedio = document.getElementById('divPromedio');
+const divMayorNota = document.getElementById('divMayorNota');
+let numeroPromedio = document.createElement("p");
+let materiasNotaAlta = [];
+let materias = ["Matematica", "Lengua", "EFSI"];
+const cambiarGif = (nota = null) =>{
+    const gif = document.getElementById('gif');
+    console.log(nota);
+    if (nota == -1) 
     {
-        valor.style.color = "red";
-        notasValidas = false;
-        alerta.innerText = "Los valores deben ser númericos, mayores a " + notaMinima + " y menores a " + notaMaxima ;
+        gif.src= "";
     }
-    else 
+    if (nota != null)
     {
-        valor.style.color = "black"; 
-        notasValidas = true;
-        alerta.innerText = "";
-    }   
-}
-
-
-formulario.addEventListener('submit', function(){
-    let notaMatematica = document.getElementById('notaMatematica');
-    let notaLengua = document.getElementById('notaLengua');
-    let notaEFSI = document.getElementById('notaEFSI');
-    const notas = [];
-    const materias=["Matemática", "Lengua", "EFSI"];
-    let promedio = 0;
-    let notaAlta = 0;
-    event.preventDefault();
-    notaMatematica = parseInt(notaMatematica);
-    notaLengua = parseInt(notaLengua);
-    notaEFSI = parseInt(notaEFSI);
-    if (!notasValidas)
-    {
-        alerta.innerText = "Los valores deben ser númericos, mayores a " + notaMinima + " y menores a " + notaMaxima
+        if (nota < 6)
+        {
+            gif.src="https://media1.tenor.com/m/jzKndvX8q54AAAAC/pikachu.gif";
+        }
+        else 
+        gif.src= "https://media.tenor.com/Jh_efPTFc4wAAAAM/andteam-andteam-ej.gif";
     }
     else
     {
-        for (let i = 0; i < document.getElementsByClassName("nota").length; i++)
-        {
-            notas[i] = (document.getElementsByClassName("nota")[i].value)
-            promedio+=parseInt(notas[i]);
-            if (notaAlta < notas[i])
-            notaAlta = notas[i];
-        }
-        mayorNota.innerText = "La/s materia/s con mayor nota es/son "
-        for (let i = 0; i < document.getElementsByClassName("nota").length; i++)
-        {
-            if (document.getElementsByClassName("nota")[i].value == notaAlta)
-            {
-                mayorNota.innerHTML = document.getElementsByClassName("nota")[i].id;
-            }
+        gif.src = "https://media1.tenor.com/m/hFvfHtYySfcAAAAC/inside-out-joy.gif"
+    }
+}
+cambiarGif();
 
-        }
-        promedioTexto.innerText = "El promedio es " + ((parseFloat)(promedio/notas.length))
+const onChange = (id) =>{
+    materiasNotaAlta=[];
+    mayorNotaTag.textContent = "";
+    promedio.innerText = "";
+    numeroPromedio.classList.add("hidden");
+    let tieneLetra = false;
+    let input = document.getElementById(id);
+    const valorNumerico = parseInt(input.value);
+    const alerta = document.getElementById("aviso" + id);
+    tieneLetra = /[a-zA-Z]/.test(input.value);
+
+    if ((isNaN(valorNumerico) || valorNumerico < notaMinima || valorNumerico > notaMaxima) || tieneLetra)
+    {
+        input.classList.add("inputInvalido");
+        input.classList.remove("inputValido");
+        notasValidas = false;
+        alerta.innerText = mensajeAlerta;
+    }
+    else 
+    {
+        input.classList.add("inputValido");
+        input.classList.remove("inputInvalido");
+        notasValidas = true;
+        alerta.innerText = "";
+    }
+    for (let i = 0; i < materias.length; i++){
+        console.log("text" + materias[i]);
+        document.getElementById("text" + materias[i]).classList.remove("azul");
+    }   
+    cambiarGif(-1);
+}
+
+
+
+botonPromedio.addEventListener("click", function(){
+    let promedio;
+    event.preventDefault();
+    tomarNotas();
+    if (!notasValidas)
+        mostrarError();
+    else{
+        alerta.classList.add("hidden");
+        promedio = (notaMatematica + notaLengua + notaEFSI) / 3;
+        if (promedio >= 6)
+            numeroPromedio.style.color = "green";
+        else
+            numeroPromedio.style.color = "red";
+        promedioTag.innerText = "El promedio es";
+        numeroPromedio.innerText = promedio;
+        divPromedio.append(numeroPromedio);
+        numeroPromedio.classList.remove("hidden");
+        cambiarGif(promedio);
     }
 })
+
+botonNota.addEventListener("click", function(){
+    let notas =[1,1,1];
+    let notaAlta = 0;
+    tomarNotas();
+    event.preventDefault();
+    if (!notasValidas)
+        mostrarError();
+    else{
+        let mensajeNota = "La/s materia/s con mejor nota son ";
+        tomarNotas(); 
+        alerta.classList.add("hidden");
+        for (let i = 0; i < document.getElementsByClassName("nota").length; i++)
+        {
+            notas[i] = (parseInt(document.getElementsByClassName("nota")[i].value))
+            console.log("la nota de " + materias[i] + " es " + notas[i]);
+            if (notaAlta < notas[i])
+            {notaAlta = notas[i];}
+        }
+        console.log(notaAlta);
+        for (let i = 0; i < notas.length; i++)
+        {
+            if (notas[i] == notaAlta)
+            {
+                mensajeNota+=materias[i];
+                materiasNotaAlta+=materias[i];
+                if (i < notas.length-1)
+                    mensajeNota+= ", ";
+                console.log("text" + materias[i]);
+                document.getElementById("text" + materias[i]).classList.add("azul");
+            }
+        }
+        if (mensajeNota[mensajeNota.length-1] == " ")
+        {
+            console.log("entró");
+            console.log(mensajeNota[mensajeNota.length-2]);
+            mensajeNota = borrarLetra(mensajeNota, mensajeNota.length-2, "");
+        }
+        mayorNotaTag.innerHTML = mensajeNota;
+        
+}})
+
+const mostrarError = () =>{
+    alerta.classList.remove("hidden");
+    alerta.innerText = mensajeAlerta;
+}
+
+const tomarNotas = () =>{
+    notaMatematica = parseInt(document.getElementById('Matematica').value);
+    notaLengua = parseInt(document.getElementById('Lengua').value);
+    notaEFSI = parseInt(document.getElementById('EFSI').value);
+}
+
+const borrarLetra = (str, index, chr) =>{
+    if(index > str.length-1) return str;
+    return str.substring(0,index) + chr + str.substring(index+1);
+}
+
+
